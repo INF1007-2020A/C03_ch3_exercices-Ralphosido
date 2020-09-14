8279934254638385b1976b798320f061200d35bc
Ralphosido <70538403+Ralphosido@users.noreply.github.com>
2020-09-14 14:32:33 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -7,7 +7,9 @@ import math
 
 def average(a: float, b: float, c: float) -> float:
     return 0.0
+moyenne = (5+4+6)/3
 
+print (moyenne)
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     return 0.0
